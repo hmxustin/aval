b19379c76df8e9b8696481dee9ce4b5251f3b97f
mx010 <hmxustin@gmail.com>
2024-06-29 12:48:41 +0300

    feat(Setup): Setup was configured
    In setup.cfg:
     - minimal config was added.

---
+++ aval/aval.py
@@ -0,0 +1,19 @@
+"""
+**Short description of this Python module**
+
+Longer description of this module.
+
+"""
+
+__author__ = "Maxim Ustin"
+__authors__ = ["Maxim Ustin"]
+__contact__ = "mxustin@gmail.com"
+__copyright__ = "Copyright © 2024, Maxim Ustin"
+__credits__ = ["Maxim Ustin"]
+__date__ = "2024"
+__deprecated__ = False
+__email__ =  "mxustin@gmail.com"
+__license__ = "MIT License"
+__maintainer__ = "Maxim Ustin"
+__status__ = "Beta"
+__version__ = "0.0.1"
